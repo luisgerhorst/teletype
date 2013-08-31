4e8c6b996698fce4a2fe513dcc89cce49c221b3e
Luis Gerhorst <luis@luisgerhorst.de>
2013-08-31 20:20:28 +0200

    initial commit, cleaned original toolbox theme

---
+++ footer.php
@@ -0,0 +1,8 @@
+	</div><!-- #main -->
+
+	<footer></footer>
+	
+	<?php wp_footer(); ?>
+
+</body>
+</html>
---
+++ functions.php
@@ -0,0 +1,207 @@
+<?php
+/**
+ * Toolbox functions and definitions
+ *
+ * Sets up the theme and provides some helper functions. Some helper functions
+ * are used in the theme as custom template tags. Others are attached to action and
+ * filter hooks in WordPress to change core functionality.
+ *
+ * When using a child theme (see http://codex.wordpress.org/Theme_Development and
+ * http://codex.wordpress.org/Child_Themes), you can override certain functions
+ * (those wrapped in a function_exists() call) by defining them first in your child theme's
+ * functions.php file. The child theme's functions.php file is included before the parent
+ * theme's file, so the child theme functions would be used.
+ *
+ * Functions that are not pluggable (not wrapped in function_exists()) are instead attached
+ * to a filter or action hook. The hook can be removed by using remove_action() or
+ * remove_filter() and you can attach your own function to the hook.
+ *
+ * For more information on hooks, actions, and filters, see http://codex.wordpress.org/Plugin_API.
+ *
+ * @package Toolbox
+ * @since Toolbox 0.1
+ */
+
+/**
+ * Set the content width based on the theme's design and stylesheet.
+ */
+if ( ! isset( $content_width ) )
+	$content_width = 640; /* pixels */
+
+if ( ! function_exists( 'toolbox_setup' ) ):
+/**
+ * Sets up theme defaults and registers support for various WordPress features.
+ *
+ * Note that this function is hooked into the after_setup_theme hook, which runs
+ * before the init hook. The init hook is too late for some features, such as indicating
+ * support post thumbnails.
+ *
+ * To override toolbox_setup() in a child theme, add your own toolbox_setup to your child theme's
+ * functions.php file.
+ */
+function toolbox_setup() {
+	
+	/**
+	 * Add default posts and comments RSS feed links to head
+	 */
+	add_theme_support( 'automatic-feed-links' );
+
+	/**
+	 * This theme uses wp_nav_menu() in one location.
+	 */
+	register_nav_menus(array(
+		'primary' => __( 'Primary Menu', 'toolbox' ),
+	));
+
+	/**
+	 * Add support for the Aside and Gallery Post Formats
+	 */
+	add_theme_support('post-formats', array('aside','image','gallery'));
+	
+}
+endif; // toolbox_setup
+
+/**
+ * Tell WordPress to run toolbox_setup() when the 'after_setup_theme' hook is run.
+ */
+add_action( 'after_setup_theme', 'toolbox_setup' );
+
+/**
+ * Set a default theme color array for WP.com.
+ */
+$themecolors = array(
+	'bg' => 'ffffff',
+	'border' => 'eeeeee',
+	'text' => '444444',
+);
+
+/**
+ * Get our wp_nav_menu() fallback, wp_page_menu(), to show a home link.
+ */
+function toolbox_page_menu_args( $args ) {
+	$args['show_home'] = true;
+	return $args;
+}
+add_filter( 'wp_page_menu_args', 'toolbox_page_menu_args' );
+
+if ( ! function_exists( 'toolbox_content_nav' ) ):
+/**
+ * Display navigation to next/previous pages when applicable
+ *
+ * @since Toolbox 1.2
+ */
+function toolbox_content_nav( $nav_id ) {
+	global $wp_query;
+
+	?>
+	<nav id="<?php echo $nav_id; ?>">
+		<h1 class="assistive-text section-heading"><?php _e('','toolbox'); ?></h1>
+
+	<?php if ( is_single() ) : // navigation links for single posts ?>
+
+		<?php previous_post_link( '<div class="nav-previous">%link</div>', '<span class="meta-nav">' . _x( '&larr;', 'Previous post link', 'toolbox' ) . '</span> %title' ); ?>
+		<?php next_post_link( '<div class="nav-next">%link</div>', '%title <span class="meta-nav">' . _x( '&rarr;', 'Next post link', 'toolbox' ) . '</span>' ); ?>
+
+	<?php elseif ( $wp_query->max_num_pages > 1 && ( is_home() || is_archive() || is_search() ) ) : // navigation links for home, archive, and search pages ?>
+
+		<?php if ( get_next_posts_link() ) : ?>
+		<div class="nav-previous"><?php next_posts_link( __( '<span class="meta-nav">&larr;</span> Older posts', 'toolbox' ) ); ?></div>
+		<?php endif; ?>
+
+		<?php if ( get_previous_posts_link() ) : ?>
+		<div class="nav-next"><?php previous_posts_link( __( 'Newer posts <span class="meta-nav">&rarr;</span>', 'toolbox' ) ); ?></div>
+		<?php endif; ?>
+
+	<?php endif; ?>
+
+	</nav><!-- #<?php echo $nav_id; ?> -->
+	<?php
+}
+endif; // toolbox_content_nav
+
+
+if ( ! function_exists( 'toolbox_posted_on' ) ) :
+/**
+ * Prints HTML with meta information for the current post-date/time and author.
+ * Create your own toolbox_posted_on to override in a child theme
+ *
+ * @since Toolbox 1.2
+ */
+function toolbox_posted_on() {
+	printf( __('<time class="entry-date" datetime="%1$s" pubdate><em>\'%2$s</em></time>', 'toolbox'), esc_attr(get_the_date('c')), esc_html(get_the_modified_date('y')));
+}
+endif;
+
+/**
+ * Adds custom classes to the array of body classes.
+ *
+ * @since Toolbox 1.2
+ */
+function toolbox_body_classes( $classes ) {
+	// Adds a class of single-author to blogs with only 1 published author
+	if ( ! is_multi_author() ) {
+		$classes[] = 'single-author';
+	}
+
+	return $classes;
+}
+add_filter( 'body_class', 'toolbox_body_classes' );
+
+/**
+ * Returns true if a blog has more than 1 category
+ *
+ * @since Toolbox 1.2
+ */
+function toolbox_categorized_blog() {
+	if ( false === ( $all_the_cool_cats = get_transient( 'all_the_cool_cats' ) ) ) {
+		// Create an array of all the categories that are attached to posts
+		$all_the_cool_cats = get_categories( array(
+			'hide_empty' => 1,
+		) );
+
+		// Count the number of categories that are attached to the posts
+		$all_the_cool_cats = count( $all_the_cool_cats );
+
+		set_transient( 'all_the_cool_cats', $all_the_cool_cats );
+	}
+
+	if ( '1' != $all_the_cool_cats ) {
+		// This blog has more than 1 category so toolbox_categorized_blog should return true
+		return true;
+	} else {
+		// This blog has only 1 category so toolbox_categorized_blog should return false
+		return false;
+	}
+}
+
+/**
+ * Flush out the transients used in toolbox_categorized_blog
+ *
+ * @since Toolbox 1.2
+ */
+function toolbox_category_transient_flusher() {
+	// Like, beat it. Dig?
+	delete_transient( 'all_the_cool_cats' );
+}
+add_action( 'edit_category', 'toolbox_category_transient_flusher' );
+add_action( 'save_post', 'toolbox_category_transient_flusher' );
+
+/**
+ * Filter in a link to a content ID attribute for the next/previous image links on image attachment pages
+ */
+function toolbox_enhanced_image_navigation( $url ) {
+	global $post, $wp_rewrite;
+
+	$id = (int) $post->ID;
+	$object = get_post( $id );
+	if ( wp_attachment_is_image( $post->ID ) && ( $wp_rewrite->using_permalinks() && ( $object->post_parent > 0 ) && ( $object->post_parent != $id ) ) )
+		$url = $url . '#main';
+
+	return $url;
+}
+add_filter( 'attachment_link', 'toolbox_enhanced_image_navigation' );
+
+
+/**
+ * This theme was built with PHP, Semantic HTML, CSS, love, and a Toolbox.
+ */
