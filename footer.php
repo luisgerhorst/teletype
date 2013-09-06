e4ff30193070f402164c2bada33dd1309a953d4d
Luis Gerhorst <luis@luisgerhorst.de>
2013-09-06 16:26:01 +0200

    custom footer
    Copyright name, footer menu, Google Analytics code

--- footer.php
+++ footer.php
@@ -1,10 +1,26 @@
+<?php
+	$options = get_option('kb_theme_options');
+?>
+	
 	</div> <!-- #content -->
+	
+	<footer>
+		
+		<p><?php printf(
+			__('&copy; %1$s %2$s', 'teletype'),
+			date('Y'),
+			$options['copyright']
+		); ?></p>
+		
+		<nav>
+			<?php wp_nav_menu(array('theme_location' => 'footer')); ?>
+		</nav>
+		
+	</footer>
 	
 	</div> <!-- #wrapper -->
 	
-	<footer>
-		<?php wp_nav_menu(array('theme_location' => 'footer')); ?>
-	</footer>
+	<?php echo $options['analytics']; ?>
 	
 	<?php wp_footer(); ?>
 
--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ if (!function_exists('teletype_setup')):
 		// menus
 		register_nav_menus(array(
 			'header' => __('Header Navigation','teletype'),
-			'footer' => __('Footer Links','teletype')
+			'footer' => __('Footer Links','teletype'),
 		));
 	
 		// thumbnails
@@ -75,7 +75,71 @@ if (!function_exists('teletype_year')):
 	
 endif;
 
-/**
- * This theme was built with PHP, Semantic HTML, CSS, love, and a toolbox.
- */
+/* via Theme Options Page für WordPress erstellen http://blog.kulturbanause.de/2011/11/theme-options-page-fur-wordpress-erstellen/ */
+
+add_action( 'admin_init', 'theme_options_init' );
+add_action( 'admin_menu', 'theme_options_add_page' );
+
+// Einstellungen registrieren (http://codex.wordpress.org/Function_Reference/register_setting)
+function theme_options_init(){
+	register_setting( 'kb_options', 'kb_theme_options', 'kb_validate_options' );
+}
+
+// Seite in der Dashboard-Navigation erstellen
+function theme_options_add_page() {
+	add_theme_page('Theme-Options', 'Theme-Options', 'edit_theme_options', 'theme-options', 'kb_theme_options_page' ); // Seitentitel, Titel in der Navi, Berechtigung zum Editieren (http://codex.wordpress.org/Roles_and_Capabilities) , Slug, Funktion 
+}
+
+// Optionen-Seite erstellen
+function kb_theme_options_page() {
+	
+	global $select_options, $radio_options;
+	if ( ! isset( $_REQUEST['settings-updated'] ) )
+	$_REQUEST['settings-updated'] = false; ?>
+
+	<div class="wrap"> 
+	
+		<?php screen_icon(); ?><h2>Theme-Options</h2>
+	
+		<?php if ( false !== $_REQUEST['settings-updated'] ) : ?> 
+			<div class="updated fade">
+				<p><strong>Options saved!</strong></p>
+			</div>
+		<?php endif; ?>
+	
+		<form method="post" action="options.php">
+			
+			<?php settings_fields('kb_options'); ?>
+			<?php $options = get_option( 'kb_theme_options' ); ?>
+			
+			<table class="form-table">
+				<tr valign="top">
+					<th scope="row">Copyright</th>
+					<td><input id="kb_theme_options[copyright]" class="regular-text" type="text" name="kb_theme_options[copyright]" value="<?php esc_attr_e( $options['copyright'] ); ?>" /></td>
+				</tr>
+				<tr valign="top">
+					<th scope="row">Google Analytics</th>
+					<td><textarea id="kb_theme_options[analytics]" class="large-text" cols="50" rows="10" name="kb_theme_options[analytics]"><?php echo esc_textarea( $options['analytics'] ); ?></textarea></td>
+				</tr>
+			</table>
+			
+			<!-- submit -->
+			<p class="submit"><input type="submit" class="button-primary" value="Save Options" /></p>
+			
+		</form>
+		
+	</div>
+	
+<?php }
+
+// Strip HTML-Code:
+// Hier kann definiert werden, ob HTML-Code in einem Eingabefeld 
+// automatisch entfernt werden soll. Soll beispielsweise im 
+// Copyright-Feld KEIN HTML-Code erlaubt werden, kommentiert die Zeile 
+// unten wieder ein. http://codex.wordpress.org/Function_Reference/wp_filter_nohtml_kses
+function kb_validate_options( $input ) {
+	// $input['copyright'] = wp_filter_nohtml_kses( $input['copyright'] );
+	return $input;
+}
+
  
