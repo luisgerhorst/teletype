3db6c3c350cdf210f70b61248fa70be1d0bc7001
Luis Gerhorst <luis@luisgerhorst.de>
2013-10-20 19:09:43 +0200

    don't show date for sticky posts

--- content.php
+++ content.php
@@ -1,4 +1,4 @@
-<article>
+<article <?php post_class(); ?>>
 
 	<header class="entry-header">
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'toolbox' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
