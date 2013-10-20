479f245f25ac5106e4f8f6fa5b63fb15fb5f0cbe
Luis Gerhorst <luis@luisgerhorst.de>
2013-10-20 13:46:51 +0200

    minor changes, improved viewport

--- header.php
+++ header.php
@@ -2,9 +2,9 @@
 <html <?php language_attributes(); ?>>
 <head>
 	
-	<meta charset="<?php bloginfo( 'charset' ); ?>" />
-	
-	<meta name="viewport" content="width=device-width" />
+	<meta charset="<?php bloginfo('charset'); ?>" />
+	<meta name="viewport" content="width=device-width,initial-scale=1" />
+	<meta name="designer" content="Luis Gerhorst">
 	
 	<title><?php
 		/*
@@ -28,11 +28,9 @@
 	
 	?></title>
 		
-	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+	<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('stylesheet_url'); ?>" />
 	
-	<!--[if lt IE 9]>
-	<script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
-	<![endif]-->
+	<!--[if lt IE 9]><script src="<?php echo get_template_directory_uri(); ?>/js/html5.min.js" type="text/javascript"></script><![endif]-->
 	
 	<?php wp_head(); ?>
 	
